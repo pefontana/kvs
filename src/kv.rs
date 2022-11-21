05bc7307f3ca8f4f49c4b907109af86fd2ce200f
pefontana <pedro.fontana@lambdaclass.com>
2022-11-20 23:33:07 -0300

    Change project organization

---
+++ src/kv.rs
@@ -0,0 +1,34 @@
+use std::{collections::HashMap, path::Path};
+
+pub struct KvStore {
+    data: HashMap<String, String>,
+}
+pub type Result<T> = std::result::Result<T, KvsError>;
+
+#[derive(Debug)]
+pub enum KvsError {
+    Error,
+}
+
+impl KvStore {
+    pub fn open(path: &Path) -> Result<KvStore> {
+        todo!()
+    }
+    pub fn new() -> KvStore {
+        KvStore {
+            data: HashMap::new(),
+        }
+    }
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
+        self.data.insert(key, value);
+        todo!()
+    }
+    pub fn get(&self, key: String) -> Result<Option<String>> {
+        self.data.get(&key).cloned();
+        todo!()
+    }
+    pub fn remove(&mut self, key: String) -> Result<()> {
+        self.data.remove(&key);
+        todo!()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,4 @@
-use std::{collections::HashMap, path::Path};
+pub use kv::KvStore;
+pub use kv::Result;
 
-use tempfile::TempDir;
-
-pub struct KvStore {
-    data: HashMap<String, String>,
-}
-pub type Result<T> = std::result::Result<T, KvsError>;
-
-#[derive(Debug)]
-pub enum KvsError {
-    Error,
-}
-
-impl KvStore {
-    pub fn open(path: &Path) -> Result<KvStore> {
-        todo!()
-    }
-    pub fn new() -> KvStore {
-        KvStore {
-            data: HashMap::new(),
-        }
-    }
-    pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        self.data.insert(key, value);
-        todo!()
-    }
-    pub fn get(&self, key: String) -> Result<Option<String>> {
-        self.data.get(&key).cloned();
-        todo!()
-    }
-    pub fn remove(&mut self, key: String) -> Result<()> {
-        self.data.remove(&key);
-        todo!()
-    }
-}
+mod kv;
